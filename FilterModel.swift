62cbd2625c8cd09af892976fa43517539f529263
ichbinme <ichbinme@users.noreply.github.com>
2021-02-21 16:05:54 +0300

    Create FilterModel.swift

---
+++ FilterModel.swift
@@ -0,0 +1,14 @@
+//
+//  FilterModel.swift
+//  MediumTest
+//
+//  Created by Mücahid Erdoğan on 21.02.2021.
+//
+
+import Foundation
+
+struct FilterModel: Identifiable {
+    var id: Int
+    var name: String
+    var selected: Bool
+}
